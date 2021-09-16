ab20f1a81b67f15f61bdc84da778aae14297cdf2
n-ermolaeva <48>
2021-09-17 01:41:50 +0300

    Create class MyThread

---
+++ src/main/java/Message.java
@@ -0,0 +1,11 @@
+public class Message {
+    public void message(String msg) {
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.print("]");
+    }
+}
---
+++ src/main/java/MyThread.java
@@ -0,0 +1,43 @@
+public class MyThread implements Runnable {
+    private final Message msg;
+    private final String str;
+    private final Thread thread;
+
+    public MyThread(Message msg,
+                    String str) {
+        this.msg = msg;
+        this.str = str;
+        thread = new Thread(this);
+    }
+
+    public static void main(String[] args) {
+        Thread tr = Thread.currentThread();
+        System.out.println("Current Thread: " + tr);
+        tr.setName("My New Thread");
+        System.out.println("After name thread: " + tr);
+
+        Message msg = new Message();
+        MyThread thread1 = new MyThread(msg, "1");
+        MyThread thread2 = new MyThread(msg, "2");
+        MyThread thread3 = new MyThread(msg, "3");
+
+        thread1.thread.start();
+        thread2.thread.start();
+        thread3.thread.start();
+
+        try {
+            thread1.thread.join();
+            thread2.thread.join();
+            thread3.thread.join();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+
+    @Override
+    public void run() {
+        synchronized (msg) {
+            msg.message(str);
+        }
+    }
+}
